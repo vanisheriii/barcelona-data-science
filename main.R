4be6744ce0578bf1650710902ab6ae82dd383f26
Kevin Vincent John  Moscoso Sotelo <kevinms1@TTC.CAT>
2023-11-17 12:20:52 +0100

    Ejecutando pruebas y nuevas funciones

--- main.R
+++ main.R
@@ -1,3 +1,32 @@
-#Primer checkou
+#Primer checkout
 "Hola Mundo"
+HolaMundo <- "Hola Mundo 2"
+HolaMundo
 
+vara <- "Hola"
+varb <- 1:100
+print(varb)
+for (i in 1:length(varb)){
+  aux <- paste(vara,varb[i], sep = "")
+  print(aux)
+}
+
+m1  <- matrix(1:12,ncol = 3,nrow = 4) #matrix
+m1
+
+m1[2,]
+m1[4,]
+m1[3,]
+
+
+d1 <- data.frame(Columna1 =  c(1,3,5,7,9),
+                 Columna2 =  c("a","b","c","d","F"),
+                 Columna3 =  c(TRUE,FALSE,TRUE,TRUE,TRUE),
+                 stringsAsFactors  = TRUE)
+
+d1
+## Apply aplica funciones a un array o suplay
+
+myFunction <- function(){
+  print("MiFuncion")
+}
