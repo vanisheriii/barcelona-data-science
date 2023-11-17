d08a8cb42163bc985dc0d4a98152c99db6656bc5
Kevin Vincent John  Moscoso Sotelo <kevinms1@TTC.CAT>
2023-11-17 17:26:45 +0100

    Pregunta 4 falta sumar

--- main.R
+++ main.R
@@ -42,32 +42,12 @@ myFunction <- function(){
 install.packages("lubridate")
 install.packages("tidyverse")
 install.packages("stringr")
+install.packages("dplyr")
 
 #Leer el archivo epa http.cvs
 library(readr)
 epa_http <- read_table("epa-http.csv", col_names = FALSE)
-#Ver el archivo
-View(epa_http)
-
-#Ver el tipo de dato de la variable epa http
-class(epa_http)
-
-## Listar la segunda columna en formato de fecha
-
-
-
-#Columna 2 
-epa_http$X2 <- as.POSIXct(epa_http$X2,format="[%d:%H:%M:%S]",tz = "utc")  
-#Define 0 si es NA
-epa_http$X7 <-  epa_http$X7 <- ifelse(is.na(epa_http$X7), 0, epa_http$X7)
-#Convierte los valores a numerico
-epa_http$X7 <-  as.numeric(epa_http$X7)
-#Calcular la media de la columna 7
-mean(epa_http$X7)
-min(epa_http$X7)
-max(epa_http$X7)
 #cambiar nombre de los columnas
-colnames(epa_http)
 colnames(epa_http)[1]<-"origen"
 colnames(epa_http)[2]<-"dataTimeStamp"
 colnames(epa_http)[3]<-"metodoHttp"
@@ -77,11 +57,62 @@ colnames(epa_http)[6]<-"respuestaHttp"
 colnames(epa_http)[7]<-"bytes"
 View(epa_http)
 
-## Pregunta 2 
-var <- stringr::str_like(epa_http$Origen,"%edu", ignore_case = TRUE)
-var
+#Ver el tipo de dato de la variable epa http
+#class(epa_http)
 
-#Primera pregunta
+## Listar la segunda columna en formato de fecha
+
+
+
+#Columna 2 convertir a formato fecha
+epa_http$dataTimeStamp <- as.POSIXct(epa_http$dataTimeStamp,format="[%d:%H:%M:%S]",tz = "utc")  
+
+#Define 0 si es NA
+epa_http$bytes <-  epa_http$bytes <- ifelse(is.na(epa_http$bytes), 0, epa_http$bytes)
+#Convierte los valores a numerico
+epa_http$bytes <-  as.numeric(epa_http$bytes)
+
+
+#Primera pregunta 1
 dim(epa_http)
 
+#Segunda pregunta calcular el promedio
+mean(epa_http$bytes)
 
+
+## Pregunta 1
+var <-   stringr::str_like(epa_http$origen,"%.edu%", ignore_case = TRUE)
+contador_true <- sum(var)
+contador_true
+
+#Pregunta 1.2
+
+var <- grepl(".edu$", epa_http$origen, ignore.case = TRUE)
+conteo <- sum(var)
+print(conteo)
+
+
+#Pregunta 4
+
+var <-   stringr::str_like(epa_http$origen,"%.edu%", ignore_case = TRUE)
+var
+
+View(epa_http)
+p4  <- epa_http[grepl(".edu$", epa_http$origen),]
+View(p4)
+unique(p4$origen)
+p4 <- p4[grepl(".txt$", p4$uri),]
+sum(p4$uri)
+View(p4)
+
+#Pregunta 4 segunda opcion
+View(epa_http)
+p42  <- dplyr::select(epa_http,origen)
+p42
+p42  <- dplyr::filter(epa_http,stringr::str_like(origen,"%edu$%",ignore_case = TRUE))
+View(p42)
+p42 <- p42[grepl(".txt$", p42$uri),]
+View(p42)
+
+
+#Pregunta 5
